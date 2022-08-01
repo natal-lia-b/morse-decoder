079cd139fbe4ae142c69617b789dd85a6e952082
natal-lia-b <80335618+natal-lia-b@users.noreply.github.com>
2022-08-02 01:32:36 +0300

    feat: done

--- src/index.js
+++ src/index.js
@@ -39,6 +39,31 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    let start = 0;
+    let result = '';
+
+    do {
+        let num = expr.substring(start, start + 10).replaceAll('00', '');
+        if (String(num) == '**********') { 
+            result += ' ';
+            start += 10;
+            continue;
+        }
+
+        let letter = '';
+        do {
+            letter = (num % 100 == 10 ? '.' : '-') + letter;
+            num = Math.floor(num / 100);
+        } while (num > 0);
+
+        if (Reflect.ownKeys(MORSE_TABLE).includes(letter)) {
+            result += MORSE_TABLE[letter];
+        }
+
+        start += 10;
+    } while (start < String(expr).length);
+
+    return result;
 }
 
 module.exports = {
